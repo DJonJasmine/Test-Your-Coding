dabec0edb3e7499cbcf6826e4a00d87ad857d66b
DJonJasmine <harrisondjon1994@gmail.com>
2022-11-11 17:15:46 -0800

    updated js file

--- script.js
+++ script.js
@@ -5,29 +5,48 @@
 *
 *
 */
- // Timer creation
-let timer = document.getElementById('timer');
 
-function countdown() {
-    let timerInterval = setInterval(function() {
-        
-    })
-}
-
+// Button animation 
 let animateButton = function(e) {
 
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
+      e.preventDefault;
+      //reset animation
       e.target.classList.remove('animate');
-    },700);
-  };
+    
+      e.target.classList.add('animate');
+      setTimeout(function(){
+      e.target.classList.remove('animate');
+      },700);
+    };
   
   let bubblyButtons = document.getElementsByClassName("btn");
   
   for (let i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
+}
+
+let timer = document.getElementById('timer');
+
+
+start.addEventListener('click', function() {
+    let timeLeft = 60;
+    function timerStart() {
+        let timerInterval = setInterval(function() {
+              timeLeft--;
+
+              timer.textContent = 'Timer: ' + timeLeft;
+
+                if (timeLeft === 0) {
+                  clearInterval(timerInterval);
+                  timer.textContent = 'Times Up!';
+                  displayMessage();
+                }
+
+        }, 1000);
+    }
+    timerStart();
+});
+
+
+
+
