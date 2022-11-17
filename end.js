55d4ce47007877d69fa479e79df77cb34713d3ea
unknown <harrisondjon1994@gmail.com>
2022-11-17 11:42:51 -0800

    updated js and html file in highscores, end, and quiz

--- end.js
+++ end.js
@@ -1,30 +1,31 @@
-let userInitials = document.querySelector('#yourname')
+let userInitials = document.querySelector('#yourInitials')
 let submitBtn = document.querySelector('#submit')
 let finalScore = document.querySelector('#finalScore')
 let mostRecentScore = localStorage.getItem('mostRecentScore')
+// placing highscore as an array
+let highscores = JSON.parse(localStorage.getItem('highscores')) || []
 
-let highscore = JSON.parse(localStorage.getItem('highScore')) || []
+let maxhighscores = 5
 
-let maxHighScores = 5
-
+// display the most recent score on the screen 
 finalScore.innerText = mostRecentScore
-
-submitBtn = e => {
+// upon submit the score will be pushed
+saveHighScore = e => {
     e.preventDefault()
 
     let score = {
-        mostRecentScore,
-        name: userInitials
+        score: mostRecentScore,
+        name: userInitials.value
     }
+   // pushing score vaule to highscore
+    highscores.push(score)
 
-    highscore.push(score)
-    highscore.sort((a,b) => {
-        return b.score = a.score
-
+    highscores.sort((a,b) => {
+        return b.score - a.score
     })
-
-    highscore.splice(5)
-
-    localStorage.setItem('highscore', JSON.stringify(highscore))
+    // will add new names in highscore
+    highscores.splice(5)
+    // retrieving highscore from an array and placing them in string
+    localStorage.setItem('highscores', JSON.stringify(highscores))
     window.location.assign('/')
 }
--- highscores.js
+++ highscores.js
@@ -1,7 +1,6 @@
 let highScoresList = document.querySelector('#highScoresList')
-let highScores = JSON.parse(localStorage.getItem('highScores')) || []
-// the local storage of your highscores
-highScoresList.innerHTML = 
-highScores.map(score => {
-    return `<li class='high-score'>${score.name} - ${score.score}</li>`
+let highscores = JSON.parse(localStorage.getItem('highscores')) || []
+// the local storage of your highscores and returns the storage to the high score list
+highScoresList.innerHTML = highscores.map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
